13d6de4a92ae4b81c59112389bcde8028e262b3d
romabozhanovgithub <67696229+romabozhanovgithub@users.noreply.github.com>
2021-12-10 12:41:24 +0300

    Update Header and Sidebar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,15 +1,25 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
 import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import MenuIcon from "@material-ui/icons/Menu";
 
-const Header = () => {
+const Header = ({ title }) => {
+    const location = useLocation();
+    
+    const sidebarHandler = () => {
+        document.getElementById("sidebar").style.left = "0px";
+    }
+
     return (
         <header>
-            <Navbar bg="light" variant="light" expand="lg">
-                <Container>
-                    <Navbar.Brand>Brand</Navbar.Brand>
-                </Container>
+            <Navbar bg="primary" variant="dark" expand="lg" className={`d-flex align-items-center ${
+                (location.pathname.includes("/login") || location.pathname.includes("/register")) ? "d-flex justify-content-center" : "d-xl-none justify-content-between"
+            }`}>
+                {!(location.pathname.includes("/login") || location.pathname.includes("/register")) && <span className="float-start sidebar-open" onClick={sidebarHandler}><MenuIcon/></span>}
+                <Navbar.Brand className="header-title" id="header-title">{title}</Navbar.Brand>
+                {!(location.pathname.includes("/login") || location.pathname.includes("/register")) && <span> </span>}
             </Navbar>
         </header>
     )
--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { Navbar, Nav } from "react-bootstrap";
+import { Accordion, Button } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import AccessTimeIcon from "@material-ui/icons/AccessTime";
 import GridOnIcon from "@material-ui/icons/GridOn";
@@ -9,41 +9,81 @@ import CalendarTodayIcon from "@material-ui/icons/CalendarToday";
 import AssessmentOutlinedIcon from "@material-ui/icons/AssessmentOutlined";
 import ListAltIcon from "@material-ui/icons/ListAlt";
 import BookmarkOutlinedIcon from "@material-ui/icons/BookmarkOutlined";
+import CloseIcon from "@material-ui/icons/Close";
+import AccountBoxOutlinedIcon from "@material-ui/icons/AccountBoxOutlined";
+import ExitToAppOutlinedIcon from "@material-ui/icons/ExitToAppOutlined";
+import { logout } from "../actions/userActions";
 import "../index.css"
 
 const Sidebar = () => {
-    useEffect(() => {
+    const location = useLocation();
 
+    const sidebarHandler = () => {
+        document.getElementById("sidebar").style.left = "-250px";
+    }
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        
     }, []);
 
+    const logoutHandler = () => {
+        dispatch(logout())
+    }
+
     return (
-        <div id="sidebar" className="sidebar shadow">
-            <span className="logo">Logo</span>
-            <NavLink to="/timetracker" activeClassName="active">
+        <div id="sidebar" className={`sidebar shadow ${(location.pathname.includes("/login") || location.pathname.includes("/register")) ? "d-none" : "d-lg-block"}`}>
+            <span className="d-flex justify-content-between">
+                <span className="logo">Logo</span>
+                <span className="d-flex justify-content-between d-xl-none sidebar-close" onClick={sidebarHandler}>
+                    <CloseIcon/>
+                </span>
+            </span>
+            <NavLink to="/timetracker" activeClassName="active" onClick={sidebarHandler}>
                 <AccessTimeIcon/>
-                <span>TIME TRACKER</span>
+                TIME TRACKER
             </NavLink>
-            <NavLink to="/timesheet" activeClassName="active">
+            <NavLink to="/timesheet" activeClassName="active" onClick={sidebarHandler}>
                 <GridOnIcon/>
-                <span>TIMESHEET</span>
+                TIMESHEET
             </NavLink>
-            <NavLink to="/calendar" activeClassName="active">
+            <NavLink to="/calendar" activeClassName="active" onClick={sidebarHandler}>
                 <CalendarTodayIcon/>
                 CALENDAR
             </NavLink>
             <span className="divider"></span>
-            <NavLink to="/dashboard" activeClassName="active">
+            <NavLink to="/dashboard" activeClassName="active" onClick={sidebarHandler}>
                 <AssessmentOutlinedIcon/>
                 DASHBOARD
             </NavLink>
-            <NavLink to="/tasks" activeClassName="active">
+            <NavLink to="/tasks" activeClassName="active" onClick={sidebarHandler}>
                 <ListAltIcon/>
                 TASKS
             </NavLink>
-            <NavLink to="/notations" activeClassName="active">
+            <NavLink to="/notations" activeClassName="active" onClick={sidebarHandler}>
                 <BookmarkOutlinedIcon/>
                 NOTATIONS
             </NavLink>
+            <span className="divider"></span>
+            <Accordion>
+                <Accordion.Item className="border-0 shadow-none" eventKey="1">
+                    <Accordion.Header>
+                        <AccountBoxOutlinedIcon/>
+                        PROFILE
+                    </Accordion.Header>
+                    <Accordion.Body className="px-0">
+                        <NavLink to="/profile" activeClassName="active" onClick={sidebarHandler}>
+                            <AccountBoxOutlinedIcon/>
+                            PROFILE
+                        </NavLink>
+                        <NavLink to="/login" activeClassName="active" onClick={logoutHandler}>
+                            <ExitToAppOutlinedIcon/>
+                            LOGOUT
+                        </NavLink>
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
         </div>
     )
 }
